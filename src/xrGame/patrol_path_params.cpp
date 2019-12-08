372d4dea4d5ce4cfc86a2f1e2617e4b83fc68ce3
Mortan <cheatmaster1@mail.ru>
2019-12-08 13:37:29 +0300

    Additional access checks for CPatrolPathParams.

--- src/xrGame/patrol_path_params.cpp
+++ src/xrGame/patrol_path_params.cpp
@@ -31,30 +31,35 @@ CPatrolPathParams::~CPatrolPathParams	()
 u32	CPatrolPathParams::count			() const
 {
 	VERIFY				(m_path);
+	if (!m_path) return 0;
 	return				(m_path->vertices().size());
 }
 
 const Fvector &CPatrolPathParams::point	(u32 index) const
 {
 	VERIFY				(m_path);
+	if (!m_path) return m_dummy;
 	VERIFY				(!m_path->vertices().empty());
 	if (!m_path->vertex(index)) {
 		ai().script_engine().script_log(eLuaMessageTypeError,"Can't get information about patrol point number %d in the patrol way %s",index,*m_path_name);
 		index			= (*m_path->vertices().begin()).second->vertex_id();
 	}
 	VERIFY				(m_path->vertex(index));
+	if (!m_path->vertex(index)) return m_dummy;
 	return				(m_path->vertex(index)->data().position());
 }
 
 u32	CPatrolPathParams::level_vertex_id	(u32 index) const
 {
 	VERIFY				(m_path->vertex(index));
+	if (!m_path->vertex(index)) return u32(-1);
 	return				(m_path->vertex(index)->data().level_vertex_id());
 }
 
 GameGraph::_GRAPH_ID CPatrolPathParams::game_vertex_id	(u32 index) const
 {
 	VERIFY				(m_path->vertex(index));
+	if (!m_path->vertex(index)) return GameGraph::_GRAPH_ID(-1);
 	return				(m_path->vertex(index)->data().game_vertex_id());
 }
 
@@ -67,31 +72,35 @@ u32	CPatrolPathParams::point			(LPCSTR name) const
 
 u32	CPatrolPathParams::point			(const Fvector &point) const
 {
+	if (!m_path->point(point)) return u32(-1);
 	return				(m_path->point(point)->vertex_id());
 }
 
 bool CPatrolPathParams::flag			(u32 index, u8 flag_index) const
 {
 	VERIFY				(m_path->vertex(index));
+	if (!m_path->vertex(index)) return false;
 	return				(!!(m_path->vertex(index)->data().flags() & (u32(1) << flag_index)));
 }
 
 Flags32 CPatrolPathParams::flags		(u32 index) const
 {
 	VERIFY				(m_path->vertex(index));
+	if (!m_path->vertex(index)) return Flags32();
 	return				(Flags32().assign(m_path->vertex(index)->data().flags()));
 }
 
 LPCSTR	CPatrolPathParams::name	(u32 index) const
 {
 	VERIFY				(m_path->vertex(index));
+	if (!m_path->vertex(index)) return "";
 	return				(*m_path->vertex(index)->data().name());
 }
 
 bool CPatrolPathParams::terminal (u32 index) const
 {
 	VERIFY				(m_path->vertex(index));
-
+	if (!m_path->vertex(index)) return false;
 	return				(m_path->vertex(index)->edges().size() == 0);
 }
 

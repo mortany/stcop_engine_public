fa4d2acdb413cb56f08a4d35a64f94cae8487065
Mortan <cheatmaster1@mail.ru>
2019-12-08 13:39:23 +0300

    Additional nullptr checks in alife object release function

--- src/xrGame/alife_simulator_script.cpp
+++ src/xrGame/alife_simulator_script.cpp
@@ -256,8 +256,10 @@ void CALifeSimulator__release					(CALifeSimulator *self, CSE_Abstract *object, 
 //	self->release						(object,true);
 
 	THROW								(object);
+	if (!object) return;
 	CSE_ALifeObject						*alife_object = smart_cast<CSE_ALifeObject*>(object);
 	THROW								(alife_object);
+	if (!alife_object) return;
 	if (!alife_object->m_bOnline) {
 		self->release					(object,true);
 		return;

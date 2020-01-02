818bf34ee8e0172dc389419ffab39e3404b271db
Иван <i-sobolevskiy@mail.ru>
2020-01-03 00:12:23 +0700

    fix physics

--- src/xrPhysics/tri-colliderknoopc/dSortTriPrimitive.h
+++ src/xrPhysics/tri-colliderknoopc/dSortTriPrimitive.h
@@ -217,14 +217,14 @@ IC int dcTriListCollider::dSortTriPrimitiveCollide (
 				debug_output().DBG_DrawTri(T,V_array,D3DCOLOR_XRGB(0,0,255));
 #endif
 			float last_pos_dist = dDOT(last_pos, tri.norm) - tri.pos;
-#ifndef _WIN64
+#ifdef _WIN64
 
-			//bool test_nan = isnan(last_pos_dist);
+			bool test_nan = isnan(last_pos_dist);
 #else
-			//constexpr bool test_nan = false;
+			constexpr bool test_nan = false;
 #endif
 
-			if ((!(last_pos_dist < 0.f)) || b_pushing /*|| test_nan*/)
+			if ((!(last_pos_dist < 0.f)) || b_pushing || test_nan)
 				if(__aabb_tri(Point(p),Point((float*)&AABB),vertices))
 				{
 #ifdef DEBUG

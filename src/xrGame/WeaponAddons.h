7793956d67f6e7630f3b57390a3eace977517250
Mortan <cheatmaster1@mail.ru>
2020-06-09 00:14:27 +0300

    Revert "Правки по пушкам"
    This reverts commit eed2ea28974f1b5c9585535f382ee6ae60ae5ec9.

--- src/xrGame/WeaponAddons.h
+++ src/xrGame/WeaponAddons.h
@@ -1,82 +1,51 @@
 #pragma once
 
-
 class WeaponAddon
 {
 public:
-			WeaponAddon(LPCSTR section);
+			WeaponAddon();
 	virtual ~WeaponAddon();
-	//---------------------------------------------------------------
-	bool	   hasIcon;
-
-	shared_str			  bone_target_name;
-	shared_str			  addon_section;
-	shared_str			  addon_name;
-
-	shared_str			  target_slot;
-	shared_str			  require;
-
-	shared_str            hud_visual;
-	shared_str            world_visual;
-
-	shared_str            anm_prefix;
-	shared_str            anm_suffix;
-
-	// Support vanilla addons & maybe gunslinger
-	xr_vector<shared_str> bones_need_enable;
-	xr_vector<shared_str> bones_need_disable;
-	xr_vector<shared_str> slots_add;
-
-	Fvector bone_offset[2][2]; // hud,world/pos,rot
-
-	// Перетащить это в прицелы, пусть отдельным типом аддонов будет
-	Fvector aim_offset[2]; // pos,rot
-
-	IKinematics*		  hud_model;
-	IKinematics*		  world_model;
-
-	u16 addon_icon[2]; //x,y
-	//---------------------------------------------------------------
-
+private:
+	shared_str bone_target_name;
+	Fmatrix bone_offset[2]; // hud,world
+	u16 slot_index;
+public:
 	virtual void save(NET_Packet& output_packet) {};
 	virtual void load(IReader& input_packet) {};
 
-	// Hud
-	void set_h_offset_pos(Fvector& vector) { bone_offset[0][0] = vector; }
-	void set_h_offset_rot(Fvector& vector) { bone_offset[0][1] = vector; }
+	void set_hud_offset(Fmatrix& matrix)
+	{
+		bone_offset[0] = matrix;
+	}
 
-	Fvector get_h_offset_pos() { return bone_offset[0][0]; }
-	Fvector get_h_offset_rot() { return bone_offset[0][1]; }
+	Fmatrix get_hud_offset()
+	{
+		return bone_offset[0];
+	}
 
-	// World
-	void set_w_offset_pos(Fvector& vector) { bone_offset[1][0] = vector; }
-	void set_w_offset_rot(Fvector& vector) { bone_offset[1][1] = vector; }
+	void set_world_offset(Fmatrix& matrix)
+	{
+		bone_offset[1] = matrix;
+	}
 
-	Fvector get_w_offset_pos() { return bone_offset[1][0]; }
-	Fvector get_w_offset_rot() { return bone_offset[1][1]; }
-
+	Fmatrix get_world_offset()
+	{
+		return bone_offset[1];
+	}
 };
 
+// Подствольник будет грузится по старой схеме
 class WAddonScope : public WeaponAddon
 {
 public:
-
-	//u8 current_mark; not used at now
-	//bool bNVenabled; // Мейби сделать возможность настраивать яркость?
-	//virtual void save(NET_Packet& output_packet);
-	//virtual void load(IReader& input_packet);
+	u8 current_mark;
+	bool bNVenabled; // Мейби сделать возможность настраивать яркость?
+	virtual void save(NET_Packet& output_packet);
+	virtual void load(IReader& input_packet);
 };
 
 class WAddonSilencer : public WeaponAddon
 {
 public:
-	//u16 durable; not used at now
-	//virtual void save(NET_Packet& output_packet);
-	//virtual void load(IReader& input_packet);
-};
-
-class WAddonLauncher : public WeaponAddon
-{
-public:
-	xr_vector<shared_str> grenade_bones; // for vanilla support
+	u16 durable;
 };
--- src/xrGame/WeaponAddonsLogic.cpp
+++ src/xrGame/WeaponAddonsLogic.cpp
@@ -1,8 +1,5 @@
-#pragma once
-
 #include "stdafx.h"
 #include "Weapon.h"
-#include "WeaponAddons.h"
 
 void CWeapon::UpdateAddonsHudParams()
 {
@@ -11,31 +8,6 @@ void CWeapon::UpdateAddonsHudParams()
 
 
 void CWeapon::UpdateAddonsTransform(bool for_hud)
-{
-
-}
-
-WeaponAddon::WeaponAddon(LPCSTR section)
-{
-	addon_section	 =	section;
-	addon_name		 =	READ_IF_EXISTS(pSettings, r_string, section, "addon_name", 0); //Если addon_name == NULL то аддон нельзя снять
-
-	bone_target_name =	pSettings->r_string(section, "target_bone");
-	target_slot		 =  pSettings->r_string(section, "slot_type");
-	require			 =  READ_IF_EXISTS(pSettings, r_string, section, "require_addon", 0); // Если NULL то требований нет
-
-	hud_visual		 =  READ_IF_EXISTS(pSettings, r_string, section, "hud_visual", 0);    // Если NULL то визуала нет, нужно для ванильных & stcop аддонов
-	world_visual	 =  READ_IF_EXISTS(pSettings, r_string, section, "world_visual", 0);
-
-	hasIcon			 =  READ_IF_EXISTS(pSettings, r_bool, section, "icon_enable", false);
-	addon_icon[0]	 =  READ_IF_EXISTS(pSettings, r_u16, section, "icon_x", 0);
-	addon_icon[1]	 =  READ_IF_EXISTS(pSettings, r_u16, section, "icon_y", 0);
-
-	anm_prefix		 =  READ_IF_EXISTS(pSettings, r_string, section, "anm_prefix", 0); // Для использования кастомных анимаций, пока так пусть будет
-	anm_suffix		 =  READ_IF_EXISTS(pSettings, r_string, section, "anm_suffix", 0);
-}
-
-WeaponAddon::~WeaponAddon()
 {
 
 }

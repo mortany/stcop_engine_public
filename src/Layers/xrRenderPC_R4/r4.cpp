12a7d0c5f960fc1645f76d456027d0d65e0aacb5
Mortan <cheatmaster1@mail.ru>
2020-08-11 01:01:51 +0300

    Откат

--- src/Layers/xrRenderPC_R4/r4.cpp
+++ src/Layers/xrRenderPC_R4/r4.cpp
@@ -460,9 +460,6 @@ void CRender::reset_begin()
 		Lights_LastFrame.clear	();
 	}
 
-	Lights.ldbTargetViewPortBuffer->rawPackageDeffered_.clear();
-	Lights.ldbTargetViewPortBuffer->rawPackage_.clear();
-
 	//AVO: let's reload details while changed details options on vid_restart
 	if (b_loaded && (dm_current_size != dm_size || ps_r__Detail_density != ps_current_detail_density))
 	{
